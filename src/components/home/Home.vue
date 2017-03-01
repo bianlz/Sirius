96ec0a044a33e5ebc87ac1d56cf2651ccca32a1d
bianlanzhou <bianlanzhou@gmail.com>
2017-03-01 18:56:24 +0800

    commit show component

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,16 +1,22 @@
 <template>
   <div class="Home">
     <h1>{{ msg }}</h1>
+    <goodsShow></goodsShow>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
+import goodsShow from 'components/common/GoodsShow'
 export default {
   name: 'Home',
   data () {
     return {
       msg: '主页'
     }
+    components: {
+	    goodsShow
+  	}
   }
 }
 </script>
