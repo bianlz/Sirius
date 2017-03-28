d806259a79abcb233bbd3fddf242c568efe91835
bianlanzhou <bianlanzhou@gmail.com>
2017-03-28 10:10:17 +0800

    add focus from interface

--- src/utils/apiUtil.js
+++ src/utils/apiUtil.js
@@ -4,7 +4,7 @@
 
  import "whatwg-fetch"
 
- const host_addr = 'http://123.56.14.172:8080/virgo/api'
+ const host_addr = 'http://123.56.14.172:8080/api'
 
 
 export let getGoodsList = function(component){
@@ -19,12 +19,36 @@ export let getGoodsList = function(component){
     return response.json();
   }).then(function(json) {
     if(json.code==10000){
-        for (var i = 0; i < 50; i++) {
-          component.items.push(json.data[1]);
+        if(json.data){
+          for (var i = 0; i < json.data.length; i++) {
+            component.items.push(json.data[1]);
+          }
         }
         if(component.$refs.my_scroller){
           component.$refs.my_scroller.finishInfinite(true)
           //component.$refs.my_scroller.resize();
+        }
+      }
+    return json;
+  });
+}
+
+export let getFocusList = function(component){
+  let response = fetch(host_addr + `/focus/list`, {
+    method: 'get',
+    mode:'cors',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }).then(function(response) {
+    return response.json();
+  }).then(function(json) {
+    if(json.code==10000){
+        if(json.data){
+          for (var i = 0; i < json.data.length; i++) {
+            component.items.push(json.data[i]);
+          }
+          console.log(component.items.length);
         }
       }
     return json;
