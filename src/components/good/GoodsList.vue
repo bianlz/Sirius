512c878a3e792be72f9b30f2a832ddcff2a4c3fb
bianlanzhou <bianlanzhou@gmail.com>
2017-04-06 17:40:15 +0800

    finish the hot list

--- src/components/good/GoodsList.vue
+++ src/components/good/GoodsList.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
     <ul class="list-group" >
-      <li v-for="item in items" class="list-group-item" >
-        <div class="goodsDiv">
-          {{ item.img }}
+        <div v-for="item in items" class="goodsDiv col-xs-2 text-left"  >
+            <img v-bind:src="item.img" alt="item" />
+            <div class="detail">
+              <h2>{{item.name}}</h2>
+              ¥ {{item.price}}/份
+            </div>
         </div>
-      </li>
     </ul>
+    <div class="listBottom">
+            
+    </div>
   </div>
 </template>
 <script>
@@ -33,6 +38,27 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
     .goodsDiv {
-      height: 300px;
+      height: 400px;
+      width: 50%;
+      border-bottom: 3px outset; 
+      padding: 0 0 0 0;
+    }
+    .goodsDiv:nth-of-type(odd){
+      border-right: 2px outset;
+    }
+    .goodsDiv:nth-of-type(even){
+      border-left: 2px outset; 
+    }
+    img {
+      height: 75%;
+      width: 100%;
+    }
+    .detail {
+      text-align: center;
+      font-size: 20px
+    }
+    .listBottom{
+      height: 800px;
+      width: 100%
     }
 </style>
--- src/utils/apiUtil.js
+++ src/utils/apiUtil.js
@@ -6,6 +6,7 @@
 
  const host_addr = 'http://123.56.14.172:8080/api'
  //const host_addr = 'http://localhost:8081/api'
+ //const host_addr = 'http://10.235.134.61:8081/api'
 
 
 export let getGoodsList = function(component){
@@ -15,19 +16,15 @@ export let getGoodsList = function(component){
     headers: {
       'Content-Type': 'application/json'
     },
-    body:''
+    body:'{"type":1}'
   }).then(function(response) {
     return response.json();
   }).then(function(json) {
     if(json.code==10000){
         if(json.data){
           for (var i = 0; i < json.data.length; i++) {
-            component.items.push(json.data[1]);
+            component.items.push(json.data[i]);
           }
-        }
-        if(component.$refs.my_scroller){
-          component.$refs.my_scroller.finishInfinite(true)
-          //component.$refs.my_scroller.resize();
         }
       }
     return json;
